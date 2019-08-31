eef2cf46bf4b98602d6996fca0b9719b10cf578c
Subham <senku36kumar@gmail.com>
2019-08-31 17:27:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/appbarexample1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.appbarexample1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.app_bar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.action_share:
+                Toast.makeText(this,"Share option selected..",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_setting:
+                Toast.makeText(this, "Setting option selected..", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+    }
+}
